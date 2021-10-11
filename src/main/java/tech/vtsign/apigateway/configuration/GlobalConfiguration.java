c16d0a08f21ef9ce38ce7ac3d156fd7aef831217
ngthotuan <ngthotuan@gmail.com>
2021-10-12 06:26:00 +0700

    update cors

--- src/main/java/tech/vtsign/apigateway/configuration/GlobalConfiguration.java
+++ src/main/java/tech/vtsign/apigateway/configuration/GlobalConfiguration.java
@@ -3,7 +3,13 @@ package tech.vtsign.apigateway.configuration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class GlobalConfiguration {
@@ -11,5 +17,18 @@ public class GlobalConfiguration {
     @LoadBalanced
     public WebClient.Builder loadBalancedWebClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setAllowedMethods(Collections.singletonList("*"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 }
